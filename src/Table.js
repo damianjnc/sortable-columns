fbe0e37c2545b28654283c243f1159c551f0e4fc
Damian Joniec <damian.joniec@iplato.com>
2019-08-07 18:07:11 +0100

    first commit

---
+++ src/Table.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+
+const Table3 = () => {
+
+    const [data, setData] = useState( [
+        {name: 'Poland', prefix: 48},
+        {name: 'Mexico', prefix: 52},
+        {name: 'UK', prefix: 44},
+        {name: 'US', prefix: 1},
+        {name: 'Italy', prefix: 39}
+    ]);
+
+    const [sorting, setSorting] = useState({ sorting: {
+            column: null,
+            direction: 'desc',
+        }});
+
+    const onSort = column  => {
+        const direction = sorting.column ? (sorting.direction === 'asc' ? 'desc' : 'asc') : 'desc';
+        const sortedData = data.sort((a, b) => {
+            if (column === 'name') {
+                const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+
+                return 0;
+            } else {
+                return a.prefix - b.prefix;
+            }
+        });
+
+        if (direction === 'desc') {
+            sortedData.reverse();
+        }
+
+        setData(sortedData);
+        setSorting({
+            column,
+            direction,
+        });
+    };
+
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th onClick={e => onSort('name', e)}>Name</th>
+                    <th onClick={e => onSort('prefix', e)}>Prefix</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(function(item, index) {
+                    console.log(index)
+                    return (
+                        <tr key={index} data-item={item}>
+                            <td data-title="Name">{item.name}</td>
+                            <td data-title="Prefix">{item.prefix}</td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+        );
+}
+
+export default Table3;
